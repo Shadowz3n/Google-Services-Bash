b1e4e2410f3d5995919bb124601efa60b0679e3c
Henrique Bissoli <emp.shad@gmail.com>
2016-01-11 14:29:45 -0200

    Update google_login.sh

--- google_login.sh
+++ google_login.sh
@@ -6,17 +6,20 @@
 BANNERT="\e[1m\e[5;32;40m"
 INVERT="\e[7m"
 RED="\e[1m\e[91m"
+VERSION='0.0.1'
 NC=`tput sgr0 && tput setab 9`
 
 banner(){
 	echo -e "\n\t${BANNERT}******************************************************************${NC}"
 	echo -e "\t${BANNERT}*                                                                *${NC}"
-	echo -e "\t${BANNERT}*                      WPExploit Version $VERSION                   *${NC}"
+	echo -e "\t${BANNERT}*               Google Services Bash Version $VERSION               *${NC}"
 	echo -e "\t${BANNERT}*               Sponsored by IMPS - http://imps.pro              *${NC}"
 	echo -e "\t${BANNERT}*                                                                *${NC}"
 	echo -e "\t${BANNERT}******************************************************************${NC}\n\n"
-	echo -e "\t${RED}${INVERT}[!]${NC} ${RED}legal disclaimer: Usage of wpexploit for attacking targets without prior mutual consent is illegal.\n\tIt is the end user's responsibility to obey all applicable local, state and federal laws.\n\tDevelopers assume no liability and are not responsible for any misuse or damage caused by this program${NC}\n"
+	echo -e "\t${RED}${INVERT}[!]${NC} ${RED}legal disclaimer: Usage of Google Services Bash for attacking targets without prior mutual consent is illegal.\n\tIt is the end user's responsibility to obey all applicable local, state and federal laws.\n\tDevelopers assume no liability and are not responsible for any misuse or damage caused by this program${NC}\n"
 }
+
+banner
 
 if [[ $1 && $2 ]]; then
 	AGENT='User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
@@ -24,5 +27,5 @@ if [[ $1 && $2 ]]; then
 	#LOGIN=`curl -L -H "$COOKIE" -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
 	echo $COOKIE
 else
-	echo "Usage: bash ./$0 <login> <pass>"
+	echo "Usage: bash $0 <login> <pass>"
 fi
