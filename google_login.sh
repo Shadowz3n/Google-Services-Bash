ee55eab4fd4249c5081960305ed9acb753ebf23d
Henrique Bissoli <emp.shad@gmail.com>
2016-01-11 12:04:18 -0200

    Create google_login.sh

---
+++ google_login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Google Services (IMPS)
+# @autor: Henrique Bissoli Silva (emp.shad@gmail.com)
+# Updates: https://github.com/Shadowz3n/Google+Services
+
+if [[ $1 && $2 ]]; then
+	AGENT='User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
+	COOKIE=`curl -I -s -H "$AGENT" "https://accounts.google.com/AddSession"`
+	#LOGIN=`curl -L -H "$COOKIE" -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
+	echo $COOKIE
+else
+	echo "Usage: bash ./$0 <login> <pass>"
+fi
