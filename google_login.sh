e2d5e28eec7be240f35d208425e8673b8b2a3dd7
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-06-08 16:23:44 -0300

    Update google_login.sh

--- google_login.sh
+++ google_login.sh
@@ -23,9 +23,9 @@ banner
 
 if [[ $1 && $2 ]]; then
 	AGENT='User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
-	COOKIE=`curl -I -s -H "$AGENT" "https://accounts.google.com/AddSession"`
-	#LOGIN=`curl -L -H "$COOKIE" -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
-	echo $COOKIE
+	curl --cookie-jar '/tmp/google_cookies.txt' -s -H "$AGENT" "https://accounts.google.com/AddSession"
+	LOGIN=`curl --cookie '/tmp/google_cookies.txt' -s -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
+	echo $LOGIN
 else
 	echo "Usage: bash $0 <login> <pass>"
 fi
