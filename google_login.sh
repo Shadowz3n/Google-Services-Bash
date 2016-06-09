8ee24ad8cfc26be8aa0027806b49df9ea43e7c67
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-06-09 00:34:10 -0300

    Update google_login.sh

--- google_login.sh
+++ google_login.sh
@@ -19,21 +19,16 @@ banner(){
 	echo -e "\t${RED}${INVERT}[!]${NC} ${RED}legal disclaimer: Usage of Google Services Bash for attacking targets without prior mutual consent is illegal.\n\tIt is the end user's responsibility to obey all applicable local, state and federal laws.\n\tDevelopers assume no liability and are not responsible for any misuse or damage caused by this program${NC}\n"
 }
 
-banner
-
-if [[ $1 && $2 ]]; then
+GoogleLogin(){
 	AGENT='User-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
 	INITIAL=`curl -s -L --cookie-jar '/tmp/google_cookies.txt' -s -H "$AGENT" "https://accounts.google.com/AddSession"`
-	#INITIAL=`echo $INITIAL | grep -Po '(?<=<form|<FORM>).*(?=</form|</FORM)'`
-	
+	INITIAL=`echo $INITIAL | grep -Po '(?<=<form|<FORM>).*(?=</form|</FORM)'`
 	GXF=`echo $INITIAL | grep -Po '(?<=gxf" value=").*(?="> <input id="profile)'`
-	#GXF=`echo $INITIAL | grep -Po '(?<=bgresponse" value=").*(?="> <input id="profile)'`
-	#TIMESTMP=`echo $INITIAL`
+	LOGIN=`curl --cookie '/tmp/google_cookies.txt' --cookie-jar '/tmp/google_cookies.txt' -s -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&Page=PasswordSeparationAddSession&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
 	
-	echo $TIMESTMP
+	echo $GXF
 	exit 1
 	
-	LOGIN=`curl --cookie '/tmp/google_cookies.txt' --cookie-jar '/tmp/google_cookies.txt' -X POST -s -H "$AGENT" "https://accounts.google.com/accountLoginInfoXhr" -d "Email=$1&Page=PasswordSeparationAddSession&requestlocation=https%3A%2F%2Faccounts.google.com%2FAddSession%23identifier"`
 	if [[ $LOGIN == *email* && $INITIAL == *name=\"gxf\"* ]]; then
 		echo -e "\t${BANNERT}Login OK${NC}"
 	
@@ -44,14 +39,18 @@ if [[ $1 && $2 ]]; then
 		PROFILE=${LOGIN##*encoded_profile_information\":\"}
 		PROFILE=${PROFILE%%\"*}
 		
-		
-		
 		PASS=`curl -L --cookie '/tmp/google_cookies.txt' --cookie-jar '/tmp/google_cookies.txt' -X POST -s -H "$AGENT" "https://accounts.google.com/AddSession" -d "Page=PasswordSeparationAddSession&timeStmp=$TIMESTMP&Email=$1&Passwd=$2&identifiertoken=&identifiertoken_audio=&identifier-captcha-input=&_utf8=☃&ProfileInformation=$PROFILE&gxf=$GXF&bgresponse=js_disabled"`
 		echo $PASS
 		#cat '/tmp/google_cookies.txt'
 	else
 		echo -e "\t${RED}Login incorreto${NC}"
 	fi
+}
+
+banner
+
+if [[ $1 && $2 ]]; then
+	GoogleLogin $1 $2
 else
 	echo "Usage: bash $0 <login> <pass>"
 fi
